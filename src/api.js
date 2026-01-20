b8d74b25670929b11a8535b618d49ce726689a62
Chinzorig Enkhtur <zorigo.enkhtur@gmail.com>
2026-01-20 08:26:21 +0100

    Url changed

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://api.weatherapi.com/v1";
+const API_BASE_URL = "https://api.weatherapi.com/v1";
 const API_KEY = "d2b9487cc15e4ae782c70937262001";
 
 export async function getForecastWeather(location, days = 3) {
