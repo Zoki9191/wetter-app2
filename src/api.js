5a0112386eaba8e5cb0782835dfc8eb39b7cc4a3
Chinzorig Enkhtur <zorigo.enkhtur@gmail.com>
2026-01-24 23:01:23 +0100

    delete btn

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = "https://api.weatherapi.com/v1";
 const API_KEY = "d2b9487cc15e4ae782c70937262001";
+const FAVORITE_CITIES_KEY = "favorite-cities";
 
 export async function getForecastWeather(location, days = 3) {
   const response = await fetch(
@@ -11,4 +12,28 @@ export async function getForecastWeather(location, days = 3) {
   console.log(weatherData);
 
   return weatherData;
+}
+
+export function getFavoriteCities() {
+  return JSON.parse(localStorage.getItem(FAVORITE_CITIES_KEY)) || [];
+}
+
+export function saveCityAsFavoriter(city) {
+  const favorites = getFavoriteCities();
+
+  if (favorites.find((favorite) => favorite === city)) {
+    alert(city + " wurde bereits den Favoriten hinzugefügt!");
+    return;
+  }
+  favorites.push(city);
+
+  localStorage.setItem(FAVORITE_CITIES_KEY, JSON.stringify(favorites));
+}
+
+export function removeCityFromFavorites(city) {
+  const favorites = getFavoriteCities();
+
+  const filteredFavorites = favorites.filter((favorite) => favorite !== city);
+
+  localStorage.setItem(FAVORITE_CITIES_KEY, JSON.stringify(filteredFavorites));
 }
--- src/detailView.js
+++ src/detailView.js
@@ -1,5 +1,9 @@
 import { rootElement } from "./main";
-import { getForecastWeather } from "./api";
+import {
+  getFavoriteCities,
+  getForecastWeather,
+  saveCityAsFavoriter,
+} from "./api";
 import {
   formatHourlyTime,
   formatTemperature,
@@ -9,15 +13,17 @@ import {
 } from "./utils";
 import { renderLoadingScreen } from "./loading";
 import { getConditionImagePath } from "./conditions";
+import { loadMainMenu } from "./mainMenu";
 
 export async function loadDetailView(cityName) {
   renderLoadingScreen("Lade Wetter für " + cityName + "...");
+  //daten fetchen
   const weatherData = await getForecastWeather(cityName);
-  renderDetailView(weatherData);
-  // eventlistener registrieren
+  renderDetailView(weatherData, cityName);
+  registerEventListeners(cityName);
 }
 
-function renderDetailView(weatherData) {
+function renderDetailView(weatherData, cityName) {
   const { location, current, forecast } = weatherData;
   const currentDay = forecast.forecastday[0];
 
@@ -31,7 +37,10 @@ function renderDetailView(weatherData) {
     rootElement.classList.add("show-background");
   }
 
+  const isFavorite = getFavoriteCities().find((city) => city === cityName);
+
   rootElement.innerHTML =
+    getActionBarHtml(!isFavorite) +
     getHeaderHtml(
       location.name,
       formatTemperature(current.temp_c),
@@ -54,6 +63,28 @@ function renderDetailView(weatherData) {
       current.precip_mm,
       current.uv,
     );
+}
+
+function getActionBarHtml(showFavoritesButton = true) {
+  const backIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+  </svg> `;
+
+  const favoriteIcon = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+      </svg> `;
+
+  return `
+      <div class="action-bar">
+          <div class="action-bar__back">${backIcon}</div>
+          ${
+            showFavoritesButton
+              ? `<div class="action-bar__favorite">${favoriteIcon}</div>`
+              : ""
+          }
+      </div> `;
 }
 
 function getHeaderHtml(location, currentTemp, condition, maxTemp, minTemp) {
@@ -217,4 +248,19 @@ function getMiniStatsHtml(
       </div>
 
    `;
+}
+
+function registerEventListeners(city) {
+  const backButton = document.querySelector(".action-bar__back");
+
+  backButton.addEventListener("click", () => {
+    loadMainMenu();
+  });
+
+  const favoriteButton = document.querySelector(".action-bar__favorite");
+
+  favoriteButton?.addEventListener("click", () => {
+    saveCityAsFavoriter(city);
+    favoriteButton.remove();
+  });
 }
